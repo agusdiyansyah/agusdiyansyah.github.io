0cc9b1a417e24af64ae9430d996480a52d199462
agusdiyansyah <kreasiin@gmail.com>
2016-01-15 21:47:39 +0700

    test project

---
+++ workspace/src/pages/home/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>agus Diyansyah</title>
+    </head>
+    <body>
+        <section class="header">
+            <?php include "./nav.php" ?>
+        </section>
+        <section class="body">
+
+        </section>
+        <section class="foother">
+
+        </section>
+    </body>
+</html>
