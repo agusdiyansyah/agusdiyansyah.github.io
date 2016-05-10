1dba4b97645a4f1aa9df871b4ed6aadbcf65f883
agusDiyansyah <kreasiin@gmail.com>
2016-05-10 13:48:03 +0700

    init idnfbs

---
+++ idnfbs/mitra.php
@@ -0,0 +1,471 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Title Page</title>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700,600' rel='stylesheet' type='text/css'>
+        <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="./assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/bootstrap-slider/css/bootstrap-slider.min.css">
+        <link rel="stylesheet" href="assets/offcanvas/css/bootstrap.offcanvas.min.css">
+        <link rel="stylesheet" href="assets/dist/css/navbar-offcanvas.css">
+
+        <link href="./assets/dist/css/main.css" rel="stylesheet">
+        <link href="./assets/dist/css/mitra.css" rel="stylesheet">
+    </head>
+    <body>
+        
+        <?php include 'part-header.php'; ?>
+
+        <section class="banner text-center">
+            <div class="container">
+            	<div class="row">
+                    <div class="col-md-12 text-right title-2">
+                        <div class="sign">
+                            <a href="./sign-in.html" class="link">Masuk</a>&nbsp atau <a href="./sign-up.html" class="btn btn-default success_btn">Daftar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="wrap">
+                            <div class="item">
+                                <h1><b>PROGRAM KEMITRAAN FBS</b></h1>
+                                <p>Cara baru untuk mendapatkan penghasilan</p>
+                                <button type="button" class="btn btn-lg success_btn">MATERI PROMOSI</button>
+                                <button type="button" class="btn btn-lg info_btn">KOMISI PARTNER</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+		<section class="komisi">
+			<div class="container">
+				<div class="row text-center new-line">
+					<div class="col-md-12">
+						<h1>Lebih Dari <span class="title-3">130.000 Mitra Dari Seluruh Dunia</span><br>Yang telah memilih FBS, Mendapatkan:</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="image"></div>
+					</div>
+					<div class="col-md-6">
+						<div class="item">
+							<div class="icon" style="background-image: url(assets/dist/images/mitra-komisi-1.png)";></div>
+							<div class="text">
+								Komisi sebesar $15 setiap lotnya untuk trading dengan EUR/USD
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(assets/dist/images/mitra-komisi-2.png)";></div>
+							<div class="text">
+								Tiga level pembayaran komisi: Anda mendapatkan 15% penghasilan dari mitra Anda di level 2, dan 5% penghasilan dari mitra Anda di level 3
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(assets/dist/images/mitra-komisi-3.png)";></div>
+							<div class="text">
+								Pendapatan tambahan tetap sebesar $500 setiap bulannya
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(assets/dist/images/mitra-komisi-4.png)";></div>
+							<div class="text">
+								Bonus dan kontes dengan hadiah yang menarik
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(assets/dist/images/mitra-komisi-5.png)";></div>
+							<div class="text">
+								Kondisi trading yang nyaman bagi mitra anda
+							</div>
+						</div>
+						<button type="button" class="btn btn-lg success_btn">MATERI PROMOSI</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="kemitraan bg-image" style="background-image: url(assets/dist/images/accounting-finance-holidays.jpg);">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-md-12">
+						<h2 class="title-2">Nyaman untuk <span class="title-3">Mendapatkan Penghasilan</span>, dan Mudah<br>Untuk <span class="title-3">Menarik Keuntungan!</span></h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-push-1 col-md-2 text-center">
+						<div class="icon" style="background-image: url(./assets/dist/images/mitra-kemitraan-1.png);"></div>
+						<div class="title-2">
+							Materi promosi yang efektif untuk menarik klien
+						</div>
+					</div>
+					<div class="col-md-push-1 col-md-2 text-center">
+						<div class="icon" style="background-image: url(./assets/dist/images/mitra-kemitraan-2.png);"></div>
+						<div class="title-2">
+							Sistem yang terbuka untuk mendapatkan komisi
+						</div>
+					</div>
+					<div class="col-md-push-1 col-md-2 text-center">
+						<div class="icon" style="background-image: url(./assets/dist/images/mitra-kemitraan-3.png);"></div>
+						<div class="title-2">
+							Bisa menarik komisi setiap hari
+						</div>
+					</div>
+					<div class="col-md-push-1 col-md-2 text-center">
+						<div class="icon" style="background-image: url(./assets/dist/images/mitra-kemitraan-4.png);"></div>
+						<div class="title-2">
+							Sistem otomatis pembayaran komisi kepada klien Anda
+						</div>
+					</div>
+					<div class="col-md-push-1 col-md-2 text-center">
+						<div class="icon" style="background-image: url(./assets/dist/images/mitra-kemitraan-5.png);"></div>
+						<div class="title-2">
+							Sistem transfer otomatis dari akun mitra ke akun klien
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<button type="button" class="btn btn-lg success_btn">KOMISI PARTNER</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="ajak double-padding">
+			<div class="container">
+				<div class="row new-line text-center">
+					<div class="col-md-12">
+						<h2>Mengajak Mitra Lain ke FBS dan <span class="title-3">Dapatkan Komisi</span> <br>
+						Dari Penghasilan Mereka</h2>
+					</div>
+				</div>
+				<div class="row new-line">
+					<div class="col-md-12 text-center">
+						<span class="calout title-2">
+							Bagaimana system 3 level dari program kemitraan FBS bekerja ?
+						</span>
+						<div class="arrow"></div>
+					</div>
+				</div>
+				<div class="row new-line">
+					<div class="col-md-12">
+						<div class="content text-center title-2">
+							<div class="item-wrap">
+								<div class="item">
+									<div class="stack-1 bg-stack-1">
+										<h3>LEVEL 1</h3>
+										Anda berada pada<br>posisi ini
+									</div>
+									<div class="arrow ar-stack-1"></div>
+									<div class="stack-2 bg-stack-2">
+										25 klien dari Anda trading minimal<br>5 lot selama satu bulan penuh
+									</div>
+									<div class="arrow ar-stack-2"></div>
+									<div class="stack-3 bg-stack-3">
+										Anda mendapat
+										<h1>$1875</h1>
+									</div>
+								</div>
+
+								<div class="item-plus"></div>
+
+								<div class="item">
+									<div class="stack-1 bg-stack-1">
+										<h3>LEVEL 2</h3>
+										Anda mendapatkan 15%<br>dari penghasilan mitra anda
+									</div>
+									<div class="arrow ar-stack-1"></div>
+									<div class="stack-2 bg-stack-2">
+										50 klien dari level 2 Anda trading<br>minimal 5 lot selama sebulan penuh
+									</div>
+									<div class="arrow ar-stack-2"></div>
+									<div class="stack-3 bg-stack-3">
+										Anda mendapat
+										<h1>$562</h1>
+									</div>
+								</div>
+
+								<div class="item-plus"><div class="image"></div></div>
+
+								<div class="item">
+									<div class="stack-1 bg-stack-1">
+										<h3>LEVEL 3</h3>
+										Anda mendapatkan $5<br>dari penghasilan mitra anda
+									</div>
+									<div class="arrow ar-stack-1"></div>
+									<div class="stack-2 bg-stack-2">
+										100 klien dari level 3 Anda trading<br>minimal 5 lot selama sebulan penuh
+									</div>
+									<div class="arrow ar-stack-2"></div>
+									<div class="stack-3 bg-stack-3">
+										Anda mendapat
+										<h1>$375</h1>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row new-line text-center samadengan">
+					<span>=</span>
+				</div>
+				<div class="row new-line">
+					<div class="col-md-12 text-center">
+						<span class="calout hasil title-2">
+							Total Penghasil Anda <br>
+							<h1>$2812</h1>
+						</span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<button type="button" class="btn btn-lg success_btn">BUKA AKUN MITRA SEKARANG</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="hitung bg-image" style="background-image: url(assets/dist/images/istock-header-accounting.jpg)">
+			<div class="container double-padding title-2">
+				<div class="row">
+					<div class="col-md-12 text-center new-line">
+						<h2 class="fw-700">Hitung Pendapatan Anda</h2>
+					</div>
+				</div>
+				<div class="row new-line">
+					<div class="col-md-push-2 col-md-8">
+						<div class="content">
+							<div class="item new-line">
+								<div class="title">Jumlah Klien</div>
+								<input type="text" class="slide slide-klien" value="" 
+									data-slider-min="" 
+									data-slider-max="100" 
+									data-slider-step="1" 
+									data-slider-value="5" 
+									data-slider-orientation="horizontal" 
+									data-slider-selection="after"
+									data-slider-tooltip="hide">
+							</div>
+							<div class="item">
+								<div class="title">Jumlah Transaksi</div>
+								<input type="text" class="slide slide-transaksi" value="" 
+									data-slider-min="" 
+									data-slider-max="100" 
+									data-slider-step="1" 
+									data-slider-value="10" 
+									data-slider-orientation="horizontal" 
+									data-slider-selection="after"
+									data-slider-tooltip="hide">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row text-center new-line">
+					<div class="col-md-12">
+						Pendapatan bulanan anda
+						<h1 class="fw-700">$1.000</h1>
+						Standar pendapatan bulanan para mitra FBS ditahun 2015 adalah <br>
+						sebesar $1.270 per bulan
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<button type="button" class="btn btn-lg success_btn">BUKA AKUN MITRA SEKARANG</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="berburu double-padding bg-2">
+			<div class="container">
+				<div class="row text-center new-line">
+					<div class="col-md-12">
+						<h2>Musim <b class="title-3">Berburu Harta Karun</b> Telah Dimulai!</h2>
+						<p>Penghasilan tambahan sebesar $500 setiap bulannya</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="image"></div>
+					</div>
+					<div class="col-md-6">
+						<h4>Lima peraturan supaya berhasil</h4>
+						<div class="item">
+							<div class="icon" style="background-image: url(./assets/dist/images/mitra-berburu-1.png);"></div>
+							<div class="text">
+								Buka akun mitra
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(./assets/dist/images/mitra-berburu-2.png);"></div>
+							<div class="text">
+								Tarik klien sebanyak - banyaknya
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(./assets/dist/images/mitra-berburu-3.png);"></div>
+							<div class="text">
+								Cek seberapa jauh Anda sudah bekerja di Personal area
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(./assets/dist/images/mitra-berburu-4.png);"></div>
+							<div class="text">
+								Penuhi misi Anda untuk menarik klien sebanyak - banyaknya
+							</div>
+						</div>
+						<div class="item">
+							<div class="icon" style="background-image: url(./assets/dist/images/mitra-berburu-5.png);"></div>
+							<div class="text">
+								Dapatkan hadiah bulanan sebesar $500!
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<button type="button" class="btn btn-lg success_btn">BUKA AKUN MITRA SEKARANG</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="penghasilan">
+			<div class="container">
+				<div class="row text-center new-line">
+					<div class="col-md-12">
+						<h2>Empat Cara untuk <span class="title-3"><b>Mendapatkan Penghasilan<br>Yang Tidak Terbatas</b></span></h2>
+					</div>
+				</div>
+				<div class="row text-center">
+					<div class="col-md-6 content">
+						<div class="row new-line">
+							<div class="col-md-6">
+								<div class="icon-wrap">
+									<div class="icon">1</div>
+								</div>
+								<div class="text">
+									Buka akun mitra dan dapatkan referral link Anda
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="icon-wrap">
+									<div class="icon">2</div>
+								</div>
+								<div class="text">
+									Sebarkan link referral Anda di website, halaman Facebook, atau di blog Anda sendiri
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="icon-wrap">
+									<div class="icon">3</div>
+								</div>
+								<div class="text">
+									pengunjung dari website/blog yang daftar lewat link referral Anda otomatis menjadi klien FBS di bawah akun mitra Anda
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="icon-wrap">
+									<div class="icon">4</div>
+								</div>
+								<div class="text">
+									FBS memberikan informasi berapa banyak penghasilan dari klien Anda
+								</div>
+							</div>
+						</div>
+						<div class="row text-center">
+							<div class="col-md-12">
+								<button type="button" class="btn btn-lg success_btn">BUKA AKUN MITRA SEKARANG</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="image"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="vip bg-2">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 image">
+					</div>
+					<div class="col-md-6 content">
+						<h2>Terbiasa Dengan <span class="title-3"><b>Layanan VIP ?</b></span></h2>
+						<p>Carilah 75 klien Dengan deposit total US$ 50.000 dan trading 750 lot, Anda akan menjadi VIP klien di FBS!</p>
+						<h4 class="fw-600">Dapatkan keistimewaan berikut:</h4>
+						<ul>
+							<li>Prioritas utama dalam proses penarikan dana</li>
+							<li>Konsultasi dengan manager kami dalam bahasa Anda</li>
+							<li>Prioritas dalam menyelesaikan semua permasalahan</li>
+							<li>Mendapatkan hadiah VIP dalam acara tertentu</li>
+							<li>Mendapatkan sertifikat VIP Partner di FBS</li>
+						</ul>
+						<button type="button" class="btn btn-lg success_btn">BUKA AKUN MITRA SEKARANG</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="pertanyaan">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-7">
+						<h2>Anda Memiliki <span class="title-3"><b>Pertanyaan ?</b></span></h2>
+						<p>
+							Semua jawaban sudah kami sediakan di layanan FAQ. Kami bisa dikontak lewat e-mail dan Facebook!
+						</p>
+						<div>
+							<a class="title-3" href=""><i class="fa fa-envelope"></i> partners@fbs.com</a>
+						</div>
+						<div>
+							<a class="title-4" href=""><i class="fa fa-facebook"></i> Halaman public untuk mitra di Facebook</a>
+						</div>
+					</div>
+					<div class="col-md-5">
+						<div class="image"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<?php include 'part-footer.php'; ?>
+
+    </body>
+	
+	<script type="text/javascript" src='assets/jquery/jquery.min.js'></script>
+	<script type="text/javascript" src='assets/bootstrap/js/bootstrap.min.js'></script>
+	<script type="text/javascript" src="assets/bootstrap-slider/bootstrap-slider.min.js"></script>
+	<script type="text/javascript" src="./assets/offcanvas/js/bootstrap.offcanvas.min.js"></script>
+	<script type="text/javascript" src="assets/waypoints/jquery.waypoints.min.js"></script>
+	<script type="text/javascript" src="assets/dist/js/main.js"></script>
+    <script type="text/javascript">
+    	$(document).ready(function() {
+    		$('.banner h1').waypoint(function() {
+	            $('.navbar').addClass('anim');
+	        });
+	        $('.banner .wrap').waypoint(function() {
+	            $('.navbar').removeClass('anim');
+	        });
+    		$(".slide").slider({
+				tooltip: 'show',
+    			formatter: function(value) {
+					return value
+				}
+    		});
+    		$('.navbar-toggle').click(function(e) {
+	            $('.navbar-header').toggleClass('black');
+	        });
+    	});
+    </script>
+</html>
---
+++ idnfbs/sign-in.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
+		<link rel="stylesheet" href="assets/dist/css/sign-in.css">
+	</head>
+	<body>
+		
+		<section class="wrap-in">
+			<div class="content">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-push-1 col-md-5 text-center">
+							<div class="image"></div>
+						</div>
+						<div class="col-md-push-2 col-md-4">
+							<form action="" method="POST" role="form">
+								<label class="input-row">Login lewat</label>
+								<div class="sosmed">
+									<a href="#" class="icon" style="background-image: url(./assets/dist/images/in-facebook.png)"></a>
+									<a href="#" class="icon" style="background-image: url(./assets/dist/images/in-tw.png)"></a>
+									<a href="#" class="icon" style="background-image: url(./assets/dist/images/in-microsoft.png)"></a>
+									<a href="#" class="icon" style="background-image: url(./assets/dist/images/in-weibo.png)"></a>
+									<a href="#" class="icon" style="background-image: url(./assets/dist/images/in-yahoo.png)"></a>
+									<a href="#" class="icon" style="background-image: url(./assets/dist/images/in-google +.png)"></a>
+								</div>
+
+								<label class="input-row">Atau login dengan akun</label>
+
+								<div class="input-row">
+									<input type="text" name="nama" id="name" class="form-control sign-input" value="" required="required" placeholder="masukkan username atau email" title="">
+								</div>
+
+								<div class="input-row">
+									<div class="input-group">
+										<input type="text" class="form-control sign-input" placeholder="masukkan password">
+										<span class="input-group-addon checkbox">
+											<label><input type="checkbox"> tampilkan</label>
+										</span>
+									</div>
+								</div>
+
+								<div class="input-row">
+									<a href="" class="btn btn-md btn-success">MASUK</a> 
+									<a href="" class="link">Lupa password</a>
+								</div>
+
+								<div>
+									<p>Klik <a href="" class="link"><b>daftar</b></a> jika belum memiliki akun</p>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-push-1 col-md-10 text-center">
+							Dapatkan keuntungan di Forex dengan adalah FBS mudah dan nyaman! Kami selalu menawarkan kondisi terbaik di pasar Forex untuk trader pemula dan trader yang sudah punya pengalaman. FBS adalah broker yang ideal untuk memulai kehidupan Anda di Forex. FBS terus berkembang dan telah menjadi pemimpin di pasar Asia. Trader kami telah mendapatkan jutaan dolar di Forex
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</body>
+</html>
